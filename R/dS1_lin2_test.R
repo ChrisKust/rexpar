bc5b7194c6183299b78eaea49dd4bd9a0c651574
ChrisKust <kustosz@statistik.tu-dortmund.de>
2014-11-03 11:16:32 +0100

    correction of level passing in CI construction

--- R/dS1_lin2_test.R
+++ R/dS1_lin2_test.R
@@ -2,6 +2,6 @@ dS1_lin2_test<-function(thetaN,alpha,y)
 {
   dS1<-rexpar::dS1_lin2(thetaN,y)
   NdS1<-sqrt(floor(length(y)/3))*(dS1-1/4)/sqrt(3/16)
-  deci<-(NdS1<qnorm(alpha))
+  deci<-(NdS1<qnorm(1-alpha))
   list(TS=NdS1,phi=deci)
 }
--- R/dS3_lin2_test.R
+++ R/dS3_lin2_test.R
@@ -2,6 +2,6 @@ dS3_lin2_test<-function(thetaN,alpha,y)
 {
   dS3<-rexpar::dS3_lin2(thetaN,y)
   NdS3<-sqrt(length(y)-2)*(dS3-1/4)/sqrt(5/16)
-  deci<-(NdS3<qnorm(alpha))
+  deci<-(NdS3<qnorm(1-alpha))
   list(TS=NdS3,phi=deci)
 }
