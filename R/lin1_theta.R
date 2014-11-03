bd17bcf96249ae986eccf14cb6188f4efe3368bf
ChrisKust <kustosz@statistik.tu-dortmund.de>
2014-11-03 17:06:36 +0100

    candidate search for linear model without intercept

---
+++ R/lin1_theta.R
@@ -0,0 +1,27 @@
+lin1_theta<-function(dat)
+{
+  dat1<-dat[1:(length(dat)-1)]
+  dat2<-dat[2:length(dat)]
+  t1<-0
+  t2<-0
+  cand_t1<-numeric(length(dat1))
+  for(i in 1:(length(dat1)))
+  {
+    cand_t1[i]<-(dat2[i]-dat1[i])/(dat1[i])#-(dat2[i]-dat1[i])/(dat1[i]))/2
+  }
+  cand_t1S<-sort(cand_t1)
+  for(i in 1:(length(cand_t1S)-1))
+  {
+    for(j in (i+1):length(cand_t1S))
+    {
+      t1<-c(t1,(cand_t1S[j]+cand_t1S[i])/2)
+    }
+  }
+  
+  t1<-c(t1)
+  
+  
+  
+  
+  list(t1=t1)
+}
