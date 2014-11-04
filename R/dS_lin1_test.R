bbbdfca956da76f4d66796b99f113bf03fb044f8
ChrisKust <kustosz@statistik.tu-dortmund.de>
2014-11-04 09:00:38 +0100

    correction in dS test

--- R/dS_lin1_test.R
+++ R/dS_lin1_test.R
@@ -2,6 +2,6 @@ dS_lin1_test<-function(thetaN,alpha,y)
 {
   dS<-rexpar::dS_lin1(thetaN,y)
   CdS<-(length(y)-1)*(dS-1/2)
-  deci<-(CdS<1/2-1/2*qchisq((1-alpha),1))
+  deci<-(CdS<1/2-1/2*qchisq((alpha),1))
   list(TS=CdS,phi=deci)
 }
