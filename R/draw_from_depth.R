1379a621fde5a682772d1ebc92fffa0248af7e54
ChrisKust <kustosz@statistik.tu-dortmund.de>
2014-12-03 11:44:46 +0100

    prediction for the linear model without intercept included (no documentation so far)

---
+++ R/draw_from_depth.R
@@ -0,0 +1,15 @@
+draw_from_depth<-function(depth,testvec,lower,upper)
+{
+  dd<-depth[(testvec>=lower) && (testvec <= upper)]
+  tsv<-testvec[(testvec>=lower) && (testvec <= upper)]
+  u<-runif(1)
+  cands_sI<-sort(cands,index.return=T)
+  depth_s<-depth[cands_sI$ix]
+  cands_s<-cands_sI$x
+  int<- sum((cands_s[2:length(cands_s)]-cands_s[1:(length(cands_s)-1)])*((depth_s[2:length(depth_s)]+depth_s[1:(length(depth_s)-1)])/2))
+  d2<-dd/int
+  d2_s<-d2[cands_sI$ix]*(c(0,cands_s[2:length(cands_s)]-cands_s[1:(length(cands_s)-1)]))
+  ind<-seq(1,length(cands_s),1)
+  theta_t<-cands_s[min(ind[cumsum(d2_s)>=u])]
+  return(theta_t)
+}
---
+++ R/predict_lin1.R
@@ -0,0 +1,47 @@
+predict_lin1<-function(y,CritLen,CritTime,NSim,alpha,restrict=F)
+{
+  cands<-lin1_theta_eps(y,0.000000001)$t1
+  unlist(lapply(cands,rexpar::dS_lin1,y=y))->depth
+  plot(cands,depth)
+  if(restrict==T)
+  {
+    theta_min<-min(cands[(length(y)-1)*(depth-1/2)>=-1/2*qchisq(1-alpha,df=1)+1/2])
+    theta_max<-max(cands[(length(y)-1)*(depth-1/2)>=-1/2*qchisq(1-alpha,df=1)+1/2])
+  }
+  else
+  {
+    theta_min<-min(cands)
+    theta_max<-max(cands)
+  }  
+  
+  ResMat<-matrix(nrow=(length(y)+3*length(y)),ncol=NSim)
+  for(i in 1:NSim)
+  {
+    y_t<-numeric(length(y)+3*length(y))
+    y_t[length(y)]<-y[length(y)]
+    theta_t<-draw_from_depth(depth,cands,theta_min,theta_max)
+    y_o<-y[1:(length(y)-1)]
+    y_p<-y[2:length(y)]
+    Res<-y_p-theta_t*y_o
+    Res_t<-sample(Res,3*length(y),replace=T)
+    
+    for(j in (length(y)+1):(length(y_t)))
+    {
+      y_t[j]<-theta_t*y_t[j-1]+Res_t[j-length(y)]
+    }
+    ResMat[,i]<-y_t
+    
+    
+  }
+  
+  abline(v=c(theta_min,theta_max),lty=2,col=2)
+  return(ResMat)
+}
+
+
+
+
+
+
+
+predict_lin1(y,alpha=0.05,restric=F,NSim=100)->rr
