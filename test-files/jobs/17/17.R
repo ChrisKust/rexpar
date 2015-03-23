61f496e9723183120dd2c2f31fb2152feb778686
ChrisKust <kustosz@statistik.tu-dortmund.de>
2015-03-23 13:11:24 +0100

    batch version of test function

---
+++ test-files/jobs/17/17.R
@@ -0,0 +1,13 @@
+Sys.sleep(0.000000)
+options(BatchJobs.on.slave = TRUE, BatchJobs.resources.path = 'C:/Users/kustosz/Documents/R/rexpar_package/rexpar/test-files/resources/resources_1426599593.RData')
+library(checkmate)
+library(BatchJobs)
+res = BatchJobs:::doJob(
+	reg = loadRegistry('C:/Users/kustosz/Documents/R/rexpar_package/rexpar/test-files'),
+	ids = c(17L),
+	multiple.result.files = FALSE,
+	disable.mail = FALSE,
+	first = 13L,
+	last = 8L,
+	array.id = NA)
+BatchJobs:::setOnSlave(FALSE)
