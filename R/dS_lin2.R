53c5b204060f530ae0b745e349b61ba1f181716d
Sebastian <szugat@statistik.tu-dortmund.de>
2015-03-27 14:04:37 +0100

    - style changes - added parameter resy for given vector of residuals

--- R/dS_lin2.R
+++ R/dS_lin2.R
@@ -1,13 +1,18 @@
-dS_lin2 <- function(theta, y, ncores = 1, model = c("linAR1", "nlinAR1", "linARc"), cpow = 1) {
+dS_lin2 <- function(theta, resy, y, ncores = 1, model = c("linAR1", "nlinAR1", "linARc"), cpow = 1) {
   model <- match.arg(model)
-  y1<-y[-1]
-  y0<-y[-length(y)]
-  if (model=="linAR1") {
-    resy <- y1 - theta[1] - theta[2] * y0
-  } else if (model=="nlinAR1") {
-    resy <- y1 - y0 - theta[1] * y0^theta[2]  
-  } else if (model=="linARc") {
-    resy <- y1 - theta[1] - theta[2] * y0^cpow
+  
+  ## if no residuals given compute them using one of the specified models:
+  if (missing(resy)) {
+    y1 <- y[-1]
+    y0 <- y[-length(y)]
+    
+    if (model == "linAR1") {
+      resy <- y1 - theta[1] - theta[2] * y0
+    } else if (model == "nlinAR1") {
+      resy <- y1 - y0 - theta[1] * y0^theta[2]  
+    } else if (model == "linARc") {
+      resy <- y1 - theta[1] - theta[2] * y0^cpow
+    }
   }
 
   Matsd <- 0 
@@ -21,7 +26,7 @@ dS_lin2 <- function(theta, y, ncores = 1, model = c("linAR1", "nlinAR1", "linARc
     ncores <- parallel::detectCores() - 1
   }
   
-  if (is.numeric(ncores) == TRUE && ncores > 1 && ncores <= parallel::detectCores() * 2) {
+  if ((is.numeric(ncores) == TRUE) && (ncores > 1) && (ncores <= parallel::detectCores() * 2)) {
     cl <- makeCluster(ncores)
     #clusterEvalQ(cl,{library(matrixcalc)})
     #clusterEvalQ(cluster,{library(parallel)})
