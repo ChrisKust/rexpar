4c80309009b030df190834c93ca4e366f81b3343
Sebastian <szugat@statistik.tu-dortmund.de>
2015-03-30 12:48:01 +0200

    - tests possible for given depth

--- R/dS2_nlin2_test.R
+++ R/dS2_nlin2_test.R
@@ -1,5 +1,7 @@
-dS2_nlin2_test<-function(thetaN, alpha, y, exact = FALSE) {
-  dS2 <- rexpar::dS2_nlin2(thetaN, y)
+dS2_nlin2_test<-function(dS2, thetaN, alpha, y, exact = FALSE) {
+  if (missing(dS2)) {
+    dS2 <- rexpar::dS2_nlin2(thetaN, y)
+  }
   NdS2 <- sqrt(floor((length(y) - 1) / 4) - 1) * (dS2 - 1/8) / sqrt(7/64)
   if (exact == TRUE) {
     deci <- ((dS2 * (floor((length(y) - 1) / 4) - 1)) <
--- R/dS3_nlin2_test.R
+++ R/dS3_nlin2_test.R
@@ -1,5 +1,7 @@
-dS3_nlin2_test <- function(thetaN, alpha, y, exact = FALSE) {
-  dS3 <- rexpar::dS3_nlin2(thetaN, y)
+dS3_nlin2_test <- function(dS3, thetaN, alpha, y, exact = FALSE) {
+  if (missing(dS3)) {
+    dS3 <- rexpar::dS3_nlin2(thetaN, y)
+  }
   NdS3 <- sqrt((length(y) - 1) - 3) * (dS3 - 1/8) / sqrt(15/64)
   if (exact == TRUE) {
     q <- quantile(exact_lim_dS3nlin(N = length(y), R = 1000), prob = alpha)
