1ee64cb2d7cc1c99bf601d38054387f0c18fe8c8
ChrisKust <kustosz@statistik.tu-dortmund.de>
2015-07-07 15:09:59 +0200

    corrections

--- R/exact_lim_dS3lin.R
+++ R/exact_lim_dS3lin.R
@@ -5,9 +5,9 @@ dSs<-numeric(R)
 for(i in 1:R)
 {
   res<-(rbinom((N-1),size=1,prob=1/2)-1/2)
-  r1<-res[seq(1,length(res)-3,1)]
-  r2<-res[seq(2,length(res)-3,1)]
-  r3<-res[seq(3,length(res)-1,1)]
+  r1<-res[seq(1,length(res),1)]
+  r2<-res[seq(2,length(res),1)]
+  r3<-res[seq(3,length(res),1)]
   m<-min(c(length(r1),length(r2),length(r3)))
   r1<-r1[1:m]
   r2<-r2[1:m]
@@ -19,12 +19,14 @@ if(plot==T)
 {
 par(mfrow=c(1,1))
 hist(dSs,plot=F,breaks=seq(min(dSs)-1,max(dSs),1))->h
-plot(h$mids,h$counts/R,type="h",,xlim=c(min(dSs),max(dSs)),ylim=c(0,0.05),xlab="value",ylab="rel. freq.")
+plot(h$mids,h$counts/R,type="h",,xlim=c(min(dSs),max(dSs)),ylim=c(0,1),xlab="value",ylab="rel. freq.")
 #h$counts/R
-rr<-rbinom(R,size=floor(N-3),prob=(1/4))
+rr<-rbinom(R,size=floor(N-2),prob=(1/4))
 floor(N/3)
 hist(rr,plot=F,breaks=seq(min(rr)-1,max(rr),1))->hh
 lines(hh$mids+0.4,hh$counts/R,type="h",col=2)
 }
 return(dSs)
 }
+
+exact_lim_dS3lin(6,10000,plot=TRUE)->ddd
